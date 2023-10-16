4ea8d5e01c56dc51139cca4982e04909a2076c1e
Mads Bentsen <madslaukslett@hotmail.com>
2023-10-16 16:14:09 +0200

    finished the assignment

---
+++ gql-queries/vol_qry.R
@@ -0,0 +1,42 @@
+## vol_qry
+
+vol_qry <- function(id, from, to) {
+    query <- sprintf('{
+    trafficData(trafficRegistrationPointId: "%s") {
+      volume {
+        byHour(from: "%s", to: "%s") {
+          edges {
+            node {
+              from
+              to
+              total {
+                volumeNumbers {
+                  volume
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }', id, from, to)
+    
+    return(query)
+  }
+
+
+
+
+#varyfing that i works by calling:
+GQL(
+  vol_qry(
+    id=stations_metadata_df$id[1], 
+    from=to_iso8601(stations_metadata_df$latestData[1],-4),
+    to=to_iso8601(stations_metadata_df$latestData[1],0)
+  ),
+  .url = configs$vegvesen_url
+)
+
+
+
+  
